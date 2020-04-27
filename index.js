ca3fcee09022add30bb25ff18a0ccee417697c10
Jerry Leung <luongvidu@gmail.com>
2020-04-27 16:57:08 +1000

    setup template engines and render code from these templates

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {res.send('Hello World!')})
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+
+
+app.get('/', function (req, res) {
+    res.render('index')
+  })
+
+app.get('/users',(req, res)=> res.render('users/index',{
+    users: [
+        {id:1, name: 'Jerry'},
+        {id:2, name: 'Jack'}
+    ]
+}))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
