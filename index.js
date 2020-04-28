a6fa63306bbc5d05c1cb1106a0f4eb5b0679cb88
Jerry Leung <luongvidu@gmail.com>
2020-04-28 10:41:53 +1000

    Connecting to small database lowdb

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//database setting
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+//instance
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+// Set some defaults
+db.defaults({ users:[] })
+  .write()
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -8,23 +17,17 @@ app.set('views', './views')
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-var users = [
-    {id:1, name: 'Jerry'},
-    {id:2, name: 'Jack'}
-]
-
-
 app.get('/', function (req, res) {
     res.render('index')
   })
 
 app.get('/users',(req, res)=> res.render('users/index',{
-    users: users
+    users: db.get('users').value()
 }))
 
 app.get('/users/search', (req,res)=>{
     var q = req.query.q
-    var matchedUsers = users.filter(user => {
+    var matchedUsers = db.get('users').value().filter(function(user){
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
     })
     res.render('users/index', {
@@ -36,7 +39,7 @@ app.get('/users/search', (req,res)=>{
 app.get('/users/create', (req,res) => res.render('users/create'))
 
 app.post('/users/create', (req,res) => {
-    users.push(req.body);
+    db.get('users').push(req.body).write();
     res.redirect('/users')
 })
 
